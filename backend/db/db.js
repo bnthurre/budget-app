a29146430a4bfafcc6686d045115282125857c2a
bnthurre <bushrahurre959@gmail.com>
2024-03-27 01:27:46 -0700

    solving bug

--- backend/db/db.js
+++ backend/db/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const db = async () => {
   try {
     mongoose.set("strictQuery", false);
-    await mongoose.connect(process.env.MONGO_URL);
+    await mongoose.connect(process.env.dbUrl);
     console.log("connedted to the database...");
   } catch (error) {
     console.log(error);
